418d8c649294e9f793df198d44c1d0b6b1a390c9
cjallow01 <114367097+cjallow01@users.noreply.github.com>
2022-11-07 21:55:02 -0600

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "5e23d07930fd20547861a201c2c920df"
+
+# Geoapify API Key
+#geoapify_key = "AIzaSyAUFI8BQWVfCPy-yBEuVkjSJ51sX6qmprg"
+
+g_key = "AIzaSyAUFI8BQWVfCPy-yBEuVkjSJ51sX6qmprg"
+
